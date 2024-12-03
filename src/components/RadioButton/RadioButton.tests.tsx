b0682d6c4c9a5bebf90125926ba5433ef714913e
Wadia Jara Quesieh <wjaraquesieh@academic.rrc.ca>
2024-12-03 11:23:24 -0600

    Testing

--- src/components/RadioButton/RadioButton.tests.tsx
+++ src/components/RadioButton/RadioButton.tests.tsx
@@ -3,13 +3,23 @@ import { render, screen } from '@testing-library/react'
 import RadioButton from './RadioButton'
 
 test('renders the radio button', () => {
-  render(<RadioButton label="Option 1" name="options" value="1" />)
+  render(
+    <RadioButton label="Option 1" name="options" value="1" checked={false} />
+  )
   const labelElement = screen.getByText(/Option 1/i)
   expect(labelElement).toBeVisible()
 })
 
 test('radio button is disabled', () => {
-  render(<RadioButton disabled label="Option 1" name="options" value="1" />)
+  render(
+    <RadioButton
+      disabled
+      label="Option 1"
+      name="options"
+      value="1"
+      checked={false}
+    />
+  )
   const inputElement = screen.getByRole('radio')
   expect(inputElement).toBeDisabled()
 })
